9fccafb83cf12fbe2a3900aec8df01d74953face
zhangzhanling <bfyxzls@sina.com>
2024-11-08 09:34:17 +0800

    添加了说明文件

--- test/hello.py
+++ test/hello.py
@@ -1,5 +1,12 @@
+import utils.tool # 导入外部包
+from utils.tool import load_text # 导入load_text方法，可直接使用方法名调用
+from utils.tool import exec_cmd
+
 def printName(name):
     print("Hello, " + name + "!")
 
-if __name__ == '__main__': #如果被其它模块引用了，那这行就不会执行了
+
+if __name__ == '__main__':  # 如果被其它模块引用了，那这行就不会执行了
     printName("测试print方法")
+    utils.tool.exec_cmd("echo 'ok'")
+    exec_cmd("ls")
---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py 示例
+__version__ = '1.0.0'
+__author__ = 'lind'
+__name__ = '工具包'
